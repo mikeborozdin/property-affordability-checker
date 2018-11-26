3822f5c1eb97f6e72b3d0b205d79ee3275309f02
Mike Borozdin <mike.borozdin@gmail.com>
2018-11-26 14:49:50 +0000

    getting recurring transactions

--- src/main/scala/com/goodlord/com/goodlord/affordabilitychecker/service/BankAccountService.scala
+++ src/main/scala/com/goodlord/com/goodlord/affordabilitychecker/service/BankAccountService.scala
@@ -1,8 +1,43 @@
 package com.goodlord.com.goodlord.affordabilitychecker.service
 
+import java.time.Month
+
 import com.goodlord.affordabilitychecker.model.Transaction
 
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
 class BankAccountService(private val transactions: List[Transaction]) {
+
+  def getRecurringTransactions(): List[Transaction] = {
+    val recurringTransactionCandidates = scala.collection.mutable.Map[String, ListBuffer[Transaction]]()
+    val transactionMonths = scala.collection.mutable.Map[String, scala.collection.mutable.Set[Month]]()
+    val uniqueMonths = mutable.Set[Month]()
+
+    for (transaction <- transactions) {
+      val transactionHash = transaction.transactionType.toString() + transaction.details + transaction.amount.toString()
+      val transactionMonth = transaction.date.getMonth()
+      uniqueMonths += transactionMonth
+
+      if(recurringTransactionCandidates.contains(transactionHash) && isDifferentMonth(transactionHash, transactionMonth, transactionMonths)) {
+        recurringTransactionCandidates(transactionHash) += transaction
+        transactionMonths(transactionHash) += transactionMonth
+      } else {
+        recurringTransactionCandidates(transactionHash) = ListBuffer(transaction)
+        transactionMonths(transactionHash) = scala.collection.mutable.Set(transactionMonth)
+      }
+    }
+
+    return recurringTransactionCandidates
+      .values
+      .filter(transactions => transactions.length == uniqueMonths.count(_ => true))
+      .flatten
+        .toList
+  }
+
+  private def isDifferentMonth(transactionHash: String, month: Month, transactionMonths: mutable.Map[String, mutable.Set[Month]]): Boolean = {
+    return !transactionMonths(transactionHash).contains(month)
+  }
 
   def getNetIncome(): BigDecimal = {
     return transactions.foldLeft(BigDecimal(0))(_ + _.amount)
--- src/test/scala/com/goodlord/affordabilitycheck/service/BankAccountServiceTest.scala
+++ src/test/scala/com/goodlord/affordabilitycheck/service/BankAccountServiceTest.scala
@@ -21,4 +21,48 @@ class BankAccountServiceTest extends FunSuite {
 
     assert(expectedNetIncome === actualNetIncome)
   }
+
+  test("Returns a list of recurring transaction based on their type, details, and amount") {
+    val expectedRecurringTransactions = List(
+      Transaction(LocalDate.of(2018, 1, 1), TransactionType.BankCredit, "Salary", 1000),
+      Transaction(LocalDate.of(2018, 2, 1), TransactionType.BankCredit, "Salary", 1000),
+      Transaction(LocalDate.of(2018, 3, 1), TransactionType.BankCredit, "Salary", 1000)
+    )
+
+    val nonRecurringTransactions = List(
+      Transaction(LocalDate.of(2018, 1, 1), TransactionType.CardPayment, "Tesco", 12.23),
+      Transaction(LocalDate.of(2018, 2, 1), TransactionType.BankCredit, "Tesco", 9.43),
+      Transaction(LocalDate.of(2018, 1, 1), TransactionType.BankCredit, "Dividends", 500),
+      Transaction(LocalDate.of(2018, 2, 1), TransactionType.BankCredit, "Dividends", 500)
+    )
+
+    val allTransactions = expectedRecurringTransactions ++ nonRecurringTransactions
+
+    val testee = new BankAccountService(allTransactions)
+
+    val actualRecurringTransactions = testee.getRecurringTransactions()
+
+    assert(actualRecurringTransactions === expectedRecurringTransactions)
+  }
+
+  test("Don't consider a transaction recurring if it occurs a few times in the same month") {
+    val expectedRecurringTransactions = List(
+      Transaction(LocalDate.of(2018, 1, 1), TransactionType.BankCredit, "Salary", 1000),
+      Transaction(LocalDate.of(2018, 2, 1), TransactionType.BankCredit, "Salary", 1000)
+    )
+
+    val nonRecurringTransactions = List(
+      Transaction(LocalDate.of(2018, 1, 1), TransactionType.BankCredit, "same transaction hash, but not recurring", 10),
+      Transaction(LocalDate.of(2018, 1, 2), TransactionType.BankCredit, "same transaction hash, but not recurring", 10),
+      Transaction(LocalDate.of(2018, 2, 2), TransactionType.BankCredit, "something else", 10)
+    )
+
+    val allTransactions = expectedRecurringTransactions ++ nonRecurringTransactions
+
+    val testee = new BankAccountService(nonRecurringTransactions)
+
+    val actualRecurringTransactions = testee.getRecurringTransactions()
+
+    assert(actualRecurringTransactions.length === 0)
+  }
 }
